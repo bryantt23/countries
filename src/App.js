be23e20a6032c48f14d9b8fdbc4feebac356e40d
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-21 15:04:19 -0800

    Fetch and show all countries using fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [input, setInput] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  // https://restcountries.eu/rest/v2/all
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setCountries(data);
+      });
+  }, []);
 
   return (
     <div className='App'>
+      find countries{' '}
       <input
         type='text'
         onChange={e => {
@@ -12,6 +24,8 @@ function App() {
         }}
       ></input>
       {input}
+      {/* {JSON.stringify(countries)} */}
+      {countries && countries.map(country => <p>{country.name}</p>)}
     </div>
   );
 }
